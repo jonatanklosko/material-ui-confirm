bde179652bfe7cf79dede3cddb6f19ef86aa1c90
Jonatan Kłosko <jonatanklosko@gmail.com>
2020-01-29 12:48:23 +0100

    Include TypeScript definitions in the final output.

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,6 @@
 import pkg from './package.json';
 import babel from 'rollup-plugin-babel';
+import copy from 'rollup-plugin-copy';
 
 export default {
   input: 'src/index.js',
@@ -9,6 +10,11 @@ export default {
   ],
   external: ['react', '@material-ui/core'],
   plugins: [
-    babel({ exclude: 'node_modules/**' })
+    babel({ exclude: 'node_modules/**' }),
+    copy({
+      targets: [
+        { src: 'src/index.d.ts', dest: 'dist' }
+      ],
+    }),
   ]
 };
